8ff3cf6c6b0b9167431426b1d49f90b54de32419
HHSNZ <hh.stobo@gmail.com>
2020-03-25 18:11:33 +1300

    add about section image

--- client/components/main/About.jsx
+++ client/components/main/About.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const About = () => {
     return (
         <div className="about-wrapper" id="about">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quisquam hic necessitatibus fugiat iusto quaerat, sunt corporis quas dolorum expedita soluta illo maxime! Assumenda veniam, dignissimos pariatur magni, earum repudiandae dolore quidem minus incidunt exercitationem molestias doloribus sunt laudantium perspiciatis ab nobis accusantium perferendis, maiores accusamus delectus blanditiis. Deserunt, nostrum?</p>
-            <p>About section, info about Jacou and her story.</p>
+            <img className="about-image" src="https://images.unsplash.com/photo-1581462696431-d14025bb6e41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Photo of studio"/>
+            <h2>My Story</h2>
+            <p>Zazou Pottery is happy to propose to you a range of hand-decorated ceramic, and handmade pottery.
+
+Welcome, I am Jacou Vettard, of Zazou Pottery Studio, Wanaka. I can create on demand that special gift you want for that special friend, or the products you would like to retail in your shop,  kiwis, ferns, sheep –  New Zealand themes.</p>
         </div>
     )
 }
