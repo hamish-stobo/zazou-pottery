766a9be4224a6758e16d3f7cd333d1a6f44d2b44
HHSNZ <hh.stobo@gmail.com>
2020-03-26 10:23:30 +1300

    minor styling, add content so services page

--- client/components/main/Services.jsx
+++ client/components/main/Services.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 const Services = () => {
     return (
         <div className="services-wrapper" id="services">
-            <p>List of services Jacou provides (pottery on demand for offices, retail stores, homes, schools)</p>
-            <p>Prices vary on a case by case basis</p>
+            <h2>Products & Services</h2>
+            <p>
+                Jacou makes beautiful, bespoke pottery. 
+                Whether for your office, home or retail store, Jacou can craft something special for you.
+                Pottery also makes for a thoughtful gift. Jacou can decorate your pot with custom messages,
+                anniversary dates and unique themes to tailor your peice to your liking.
+                <a href="#contact"> Contact Jacou</a> to request a custom peice, or pick something from the <a href="#gallery">catalogue:</a> 
+            </p>
         </div>
     )
 }
