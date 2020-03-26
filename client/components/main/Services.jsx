720f62237419ad0a975b5ccfec1aa7031c45ddeb
HHSNZ <hh.stobo@gmail.com>
2020-03-27 12:16:21 +1300

    slider component almost complete

--- client/components/main/Services.jsx
+++ client/components/main/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
             <p>
                 Jacou makes beautiful, bespoke pottery. 
                 Whether for your office, home or retail store, Jacou can craft something special for you.
-                Pottery also makes for a thoughtful gift. Jacou can decorate your pot with custom messages,
+                Pottery also makes for a thoughtful gift. Jacou can decorate your peice with custom messages,
                 anniversary dates and unique themes to tailor your peice to your liking.
                 <a href="#contact"> Contact Jacou</a> to request a custom peice, or pick something from the <a href="#gallery">catalogue:</a> 
             </p>
--- client/components/main/slider/Slider.jsx
+++ client/components/main/slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import SliderContent from './SliderContent'
 import Slide from './Slide.jsx'
 import SliderArrow from './SliderArrow'
@@ -7,50 +7,100 @@ import Dots from './Dots.jsx'
 const slides = [
     'https://images.unsplash.com/photo-1523554888454-84137e72c3ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
     'https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1415&q=80',
-    'https://images.unsplash.com/photo-1526304760382-3591d3840148?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    'https://images.unsplash.com/photo-1526304760382-3591d3840148?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1519233181562-35b0b8bb2b1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1553523292-1140e57d4f87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    'https://images.unsplash.com/photo-1552344284-834039a82932?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    'https://images.unsplash.com/photo-1576873866239-7d7fd7402e1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
+    'https://images.unsplash.com/photo-1583655446990-0e21c2bfb7f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'
 ]
 
 const getWidth = () => window.innerWidth
 
 const Slider = () => {
+
+    const firstSlide = slides[0]
+    const secondSlide = slides[1]
+    const lastSlide = slides[slides.length - 1]
+
     const [state, setState] = useState({
         activeSlide: 0,
-        translate: 0,
-        transition: 0.45
+        translate: getWidth(),
+        transition: 0.45,
+        _slides: [lastSlide, firstSlide, secondSlide]
       })
-    const { activeSlide, translate, transition } = state
 
-    const nextSlide = () => {
-        if (activeSlide === slides.length - 1) {
-          return setState({
-            ...state,
-            translate: 0,
-            activeSlide: 0
-          })
+    const { activeSlide, translate, transition, _slides } = state
+
+    const transitionRef = useRef()
+    const resizeRef = useRef()
+
+    useEffect(() => {
+      transitionRef.current = smoothTransition
+      resizeRef.current = handleResize
+    })
+
+    useEffect(() => {
+
+      const smooth = e => {
+        if (e.target.className.includes('slider-content')) {
+          transitionRef.current()
         }
-    
-        setState({
-          ...state,
-          activeSlide: activeSlide + 1,
-          translate: (activeSlide + 1) * getWidth()
-        })
       }
-    
-      const prevSlide = () => {
-        if (activeSlide === 0) {
-          return setState({
-            ...state,
-            translate: (slides.length - 1) * getWidth(),
-            activeSlide: slides.length - 1
-          })
-        }
-    
-        setState({
-          ...state,
-          activeSlide: activeSlide - 1,
-          translate: (activeSlide - 1) * getWidth()
-        })
+
+      const resize = () => {
+        resizeRef.current()
       }
+
+      const transitionEnd = window.addEventListener('transitionend', smooth)
+      const onResize = window.addEventListener('resize', resize)
+
+      return () => {
+        window.removeEventListener('transitionend', transitionEnd)
+        window.removeEventListener('resize', onResize)
+      }
+    }, [])
+
+    useEffect(() => {
+      if (transition === 0) setState({ ...state, transition: 0.45 })
+    }, [transition])
+  
+    const handleResize = () => {
+      setState({ ...state, translate: getWidth(), transition: 0 })
+    }
+  
+    const smoothTransition = () => {
+      let _slides = []
+  
+      // We're at the last slide.
+      if (activeSlide === slides.length - 1)
+        _slides = [slides[slides.length - 2], lastSlide, firstSlide]
+      // We're back at the first slide. Just reset to how it was on initial render
+      else if (activeSlide === 0) _slides = [lastSlide, firstSlide, secondSlide]
+      // Create an array of the previous last slide, and the next two slides that follow it.
+      else _slides = slides.slice(activeSlide - 1, activeSlide + 2)
+  
+      setState({
+        ...state,
+        _slides,
+        transition: 0,
+        translate: getWidth()
+      })
+    }
+    
+    const nextSlide = () =>
+    setState({
+      ...state,
+      translate: translate + getWidth(),
+      activeSlide: activeSlide === slides.length - 1 ? 0 : activeSlide + 1
+    })
+
+  const prevSlide = () =>
+    setState({
+      ...state,
+      translate: 0,
+      activeSlide: activeSlide === 0 ? slides.length - 1 : activeSlide - 1
+    })
 
       const dotClick = (idx) => {
           setState({
@@ -64,9 +114,9 @@ const Slider = () => {
             <SliderContent 
                 translate={translate}
                 transition={transition}
-                width={getWidth() * slides.length} 
+                width={getWidth() * _slides.length} 
             >
-            {slides.map((slide, idx) => (
+            {_slides.map((slide, idx) => (
                 <Slide width={getWidth()} key={slide + idx} content={slide} />
             ))}
             </SliderContent>
