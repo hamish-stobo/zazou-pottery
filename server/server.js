499bffca857aa4e0c876f6a24dcabf709f3cbcfe
HHSNZ <hh.stobo@gmail.com>
2020-03-28 12:49:53 +1300

    being configuring nodemailer to send emails to gmail

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const cors = require('cors')
+const nodemailer = require('nodemailer');
+require('dotenv').config()
 
 const app = express()
 
@@ -8,10 +10,49 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, './public')))
 app.use(cors())
 
-app.post('/send', (req, res) => {
-    console.log('req')
-    res.statusMessage = 'success'
-    res.status(200).end()
-})
+const transport = {
+    host: 'imap.gmail.com',
+    port: 993,
+    auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS
+  }
+}
+
+const transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Server is ready to take messages');
+    }
+  });
+  
+app.post('/send', (req, res, next) => {
+    const name = req.body.name
+    const email = req.body.email
+    const message = req.body.message
+    const content = `name: ${name} \n email: ${email} \n message: ${message} `
+  
+    const mail = {
+      from: name,
+      to: 'hh.stobo@gmail.com',  // Change to email address that you want to receive messages on
+      subject: 'Request from Zazou website',
+      text: content
+    }
+  
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        res.json({
+          status: 'fail'
+        })
+      } else {
+        res.json({
+         status: 'success'
+        })
+      }
+    })
+  })
 
 module.exports = app
