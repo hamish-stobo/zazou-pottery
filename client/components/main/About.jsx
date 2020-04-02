f97de13d7e7e829316303edcdd2ea055da33c322
HHSNZ <hh.stobo@gmail.com>
2020-04-03 09:10:53 +1300

    slide in on scroll working noicely

--- client/components/main/About.jsx
+++ client/components/main/About.jsx
@@ -6,17 +6,17 @@ const About = () => {
         <div className="about-wrapper" id="about">
             {/* <div id="about-trigger"/> */}
             <Controller>
-            <Scene offset={-100} triggerElement="#about img" duration={700} classToggle={['.adefault', 'fade-mainEls-in']} reverse={true} indicators={true}>
+            <Scene triggerHook={0.7} triggerElement="#about img" duration={700} classToggle={['.adefault', 'fade-mainEls-in']} reverse={true} indicators={true}>
                 {() => (
                 <img className="about-image adefault" src="http://zazoupottery.co.nz/highslide/images/large/25348280_1926057704101055_1206403772006483410_n.jpg" alt="Photo of studio"/>
             )}
             </Scene>
-            <Scene offset={-100} triggerElement="#about h2" duration={700} classToggle={['.adefault1', 'fade-mainEls-in']} reverse={true} indicators={true}>
+            <Scene triggerHook={0.7} triggerElement="#about h2" duration={700} classToggle={['.adefault1', 'fade-mainEls-in']} reverse={true} indicators={true}>
                 {() => (
                <h2 className="adefault1">My Story</h2>
             )}
             </Scene>
-            <Scene offset={-100} triggerElement="#about p" duration={700} classToggle={['.adefault3', 'fade-mainEls-in']} reverse={true} indicators={true}>
+            <Scene triggerHook={0.7} triggerElement="#about p" duration={700} classToggle={['.adefault3', 'fade-mainEls-in']} reverse={true} indicators={true}>
                 {() => (
                 <p className="adefault3">Zazou Pottery Studio was started in 808BC. 
                 A labour of love, Jacou pours her passion and creativity into her work, 
