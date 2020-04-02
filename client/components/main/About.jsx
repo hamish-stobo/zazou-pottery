44ea87846894c6b1eb2b5838874fad989be9e199
HHSNZ <hh.stobo@gmail.com>
2020-04-02 16:20:47 +1300

    if it works it works - start implementing fade-in on scroll

--- client/components/main/About.jsx
+++ client/components/main/About.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
+import { Controller, Scene } from 'react-scrollmagic'
 
 const About = () => {
     return (
         <div className="about-wrapper" id="about">
-            <img className="about-image" src="http://zazoupottery.co.nz/highslide/images/large/25348280_1926057704101055_1206403772006483410_n.jpg" alt="Photo of studio"/>
-            <h2>My Story</h2>
-            <p>Zazou Pottery Studio was started in 808BC. 
+            {/* <div id="about-trigger"/> */}
+            <Controller>
+            <Scene offset={-100} triggerElement="#about img" duration={700} classToggle={['.adefault', 'fade-mainEls-in']} reverse={true} indicators={true}>
+                {() => (
+                <img className="about-image adefault" src="http://zazoupottery.co.nz/highslide/images/large/25348280_1926057704101055_1206403772006483410_n.jpg" alt="Photo of studio"/>
+            )}
+            </Scene>
+            <Scene offset={-100} triggerElement="#about h2" duration={700} classToggle={['.adefault1', 'fade-mainEls-in']} reverse={true} indicators={true}>
+                {() => (
+               <h2 className="adefault1">My Story</h2>
+            )}
+            </Scene>
+            <Scene offset={-100} triggerElement="#about p" duration={700} classToggle={['.adefault3', 'fade-mainEls-in']} reverse={true} indicators={true}>
+                {() => (
+                <p className="adefault3">Zazou Pottery Studio was started in 808BC. 
                 A labour of love, Jacou pours her passion and creativity into her work, 
                 and with over 293 years of experience, she has perfected her craft. <br/>The studio, situated in beautiful Wanaka, New Zealand, houses many examples
                 of Jacou's work. Blah blah blah good info about the studio give the readers
                 a sense of authenticity.
-            </p>
-
+                </p>
+            )}
+            </Scene>
+            </Controller>
         </div>
     )
 }
