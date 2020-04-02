0ed930ca5edf80a30b50f7427a23e4681cc5b31b
HHSNZ <hh.stobo@gmail.com>
2020-04-02 14:51:42 +1300

    Revert "start implementing parallax scrolling effects"
    This reverts commit d4bf9f99273b7c7bdd4afa28fbf97e26b4479c09.

--- client/index.js
+++ client/index.js
@@ -2,14 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/App'
-import { ParallaxProvider } from 'react-skrollr'
 
-ReactDOM.render(
-    <ParallaxProvider
-        init={{
-          smoothScrollingDuration: 500,
-          smoothScrolling: true,
-          forceHeight: false
-        }}
-      ><App /></ParallaxProvider>
-    , document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById("app"))
