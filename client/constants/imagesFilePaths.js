1a68342127fb8567bacd0de1af4523e9437bb1d5
HHSNZ <hh.stobo@gmail.com>
2020-04-04 10:49:15 +1300

    images moved to local directory

---
+++ client/constants/imagesFilePaths.js
@@ -0,0 +1,14 @@
+const slides = [
+    "images/catalogue/chillinsanta.jfif",
+    "images/catalogue/crazyglass.jfif",
+    "images/catalogue/cuplineup.jfif",
+    "images/catalogue/leafypots.jfif",
+    "images/catalogue/plantpots.jfif",
+    "images/catalogue/plates.jfif",
+    "images/catalogue/platesandcutlery.jfif",
+    "images/catalogue/russiandollsbutnot.jfif",
+    "images/catalogue/spotoftea.jfif",
+    "images/catalogue/vase.jfif"
+]
+
+export default slides
