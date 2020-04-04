4fc27624178943d8b7acbfb92eea45fb3c4b6a1d
HHSNZ <hh.stobo@gmail.com>
2020-04-04 15:09:11 +1300

    change top section so no slideshow

--- client/components/main/top/Top.jsx
+++ client/components/main/top/Top.jsx
@@ -6,13 +6,14 @@ const Top = ({selected}) => {
         <div className="top-wrapper">
             <h1 className="top-header fade-in">Z<span className="top-header-text">AZOU</span> P<span className="top-header-text">OTTERY</span> S<span className="top-header-text">TUDIO</span></h1>
             <Arrow selected={selected}/>
-            <ul className="slideshow">
+            <div className="top-cover fade-in"> </div>
+            {/* <ul className="slideshow fade-in"> */}
+                {/* <li></li> */}
+                {/* <li></li>
                 <li></li>
                 <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+                <li></li> */}
+            {/* </ul> */}
         </div>
     )
 }
